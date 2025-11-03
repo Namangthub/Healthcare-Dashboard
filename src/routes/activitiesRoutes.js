3197067001d5841a6bbf77844c8ab1dac959f533
Naman <146616222+Namangthub@users.noreply.github.com>
2025-11-03 17:25:25 +0530

    fixed activities api

--- src/routes/activitiesRoutes.js
+++ src/routes/activitiesRoutes.js
@@ -4,6 +4,6 @@ import { ActivitiesController } from '../controllers/activitiesController.js';
 const router = express.Router();
 
 // Get recent activities
-router.get('/recent', ActivitiesController.getRecentActivities);
+router.get('/', ActivitiesController.getRecentActivities);
 
 export default router;
