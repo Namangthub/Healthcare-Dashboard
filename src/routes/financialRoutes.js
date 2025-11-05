eb0e2c209528241a95d957819ff3d92f6c6aa8fa
Aman Raturi <amanraturi2404@gmail.com>
2025-11-05 13:48:17 +0530

    fixed month to monthly

--- src/routes/financialRoutes.js
+++ src/routes/financialRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // ✅ Routes for GET only
 router.get('/', FinancialController.getAll);
 router.get('/year/:year', FinancialController.getByYear);
-router.get('/summary/month/year/:year', FinancialController.getMonthlySummary);
+router.get('/summary/monthly/year/:year', FinancialController.getMonthlySummary);
 router.get('/summary/quarter/year/:year', FinancialController.getQuarterlySummary);
 router.get('/summary/year/:year', FinancialController.getYearlySummary);
 router.get('/department/:id', FinancialController.getByDepartment);
